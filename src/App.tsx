66c9e2598112417b1f92e3de6ffb6bfbf4325671
Aline Carvalho <alinecarvalho.pro@gmail.com>
2023-08-17 16:18:09 -0300

    feat: initial settings, fonts, icons and images

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Ho'oponopono</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
